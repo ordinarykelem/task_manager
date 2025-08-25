a3c9465d1bbb91144011467eece341c164164851
ordinarykelem <kelembampoeaddo@gmail.com>
2025-08-25 07:52:08 -0700

    venv created and pymongo installed

--- # Ask user to input their purchase amoun.py
+++
@@ -1,7 +0,0 @@
-# Ask user to input their purchase amount as float
-purchase_amount = float(input("Enter your purchase amount in cedis: "))
-# If the purchase is 100 cedis or more apply 20% discount and print
-if purchase_amount >= 100:
-# If the purchase is 50 cedis or more apply 10% and print
-# If the purchase is less than 50 cedis apply no discount and printif amount >= 100:
-
--- personal.py
+++
@@ -1,5 +0,0 @@
-age = input("How old are you?n")
-if int(age) >= 18 and int(age) <= 45:
-    print("You have access.")
-else:
- print("You are not allowed here.")
